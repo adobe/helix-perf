164d39139fce308e8ef034032e043e8a1be79f2e
Lars Trieloff <trieloff@adobe.com>
2019-01-17 14:27:08 +0000

    Basic calibre client

--- src/index.js
+++ src/index.js
@@ -9,7 +9,36 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+function init(token) {
+  process.env.CALIBRE_API_TOKEN = token;
+  let calibre;
+  return () => {
+    if (calibre) {
+      return calibre;
+    }
+    calibre = require('calibre');
+  }
+}
 
-module.exports = async function main() {
-  return 'Hello, world.';
+function test(calibre, {url, location, device, connection, strain}) {
+  return calibre.Test.create({
+    url,
+    location,
+    device,
+    connection,
+    cookies: [{
+      name: 'X-Strain',
+      value: strain,
+      secure: true,
+      httpOnly: true
+    }]
+  }).then(({ uuid }) => calibre.Test.waitForTest(uuid));
+}
+
+module.exports = async function main({
+  CALIBRE_AUTH,
+  tests = []
+}) {
+  const getcalibre = init(CALIBRE_AUTH);
+  return Promise.all(tests.map((spec) => test(getcalibre(), spec)));
 };
