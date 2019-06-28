18dc325b2b9ea190dc109e6886835512eec00f56
Lars Trieloff <trieloff@adobe.com>
2019-06-28 07:34:34 +0000

    fix(monitoring): use helix-pingdom-status for health check
    fixes #27

--- index.js
+++ index.js
@@ -10,22 +10,9 @@
  * governing permissions and limitations under the License.
  */
 
+const { wrap } = require('@adobe/helix-pingdom-status');
 const main = require('./src/index');
 
-module.exports.main = (params) => {
-  const start = Date.now();
-  // eslint-disable-next-line no-underscore-dangle
-  if (params && params.__ow_method && params.__ow_method === 'get') {
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/xml',
-      },
-      body: `<pingdom_http_custom_check>
-        <status>OK</status>
-        <response_time>${Math.abs(Date.now() - start) / 1000}</response_time>
-    </pingdom_http_custom_check>`,
-    };
-  }
-  return main(params);
-};
+module.exports.main = wrap(main, {
+  calibre: 'https://calibreapp.com/graphql'
+})
