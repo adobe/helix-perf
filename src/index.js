04224658702b006315954ec4b1950179ec3d7cad
Tobias Bocanegra <tripod@adobe.com>
2020-01-23 14:21:52 +0900

    fix(epsagon): use helix-epsagon wrapper

--- src/index.js
+++ src/index.js
@@ -12,31 +12,8 @@
 const { wrap: status } = require('@adobe/helix-status');
 const { wrap } = require('@adobe/openwhisk-action-utils');
 const { logger } = require('@adobe/openwhisk-action-logger');
+const { epsagon } = require('@adobe/helix-epsagon');
 const perf = require('./perf.js');
-
-/**
- * Instruments the action with epsagon, if a EPSAGON_TOKEN is configured.
- */
-function epsagon(action) {
-  return async (params) => {
-    const { __ow_logger: log } = params;
-    if (params && params.EPSAGON_TOKEN) {
-      // ensure that epsagon is only required, if a token is present.
-      // this is to avoid invoking their patchers otherwise.
-      // eslint-disable-next-line global-require
-      const { openWhiskWrapper } = require('epsagon');
-      log.info('instrumenting epsagon.');
-      // eslint-disable-next-line no-param-reassign
-      action = openWhiskWrapper(action, {
-        token_param: 'EPSAGON_TOKEN',
-        appName: 'Helix Services',
-        metadataOnly: false, // Optional, send more trace data,
-        ignoredKeys: [/[A-Z0-9_]+/],
-      });
-    }
-    return action(params);
-  };
-}
 
 /**
  * Main function called by the openwhisk invoker.
